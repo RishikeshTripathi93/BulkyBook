7da9c8d83fad57279d6f016e888a7cb41751d63e
RishikeshTripathi93 <rishikesh.t12345@gmail.com>
2024-07-25 16:00:52 +0530

    Add files via upload

---
+++ Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Repository.IRepository
+{
+   
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+       
+    }
+}
